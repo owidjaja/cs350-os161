15b542a35fd6fb1841385b4db48ffee415d9d848
owidjaja <owidjaja@connect.ust.hk>
2022-01-25 03:50:38 -0500

    add sort.c, move user code to scripts/

---
+++ scripts/sort.c
@@ -0,0 +1,71 @@
+#include <stdbool.h>
+#include <limits.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// https://stackoverflow.com/questions/1787996/c-library-function-to-perform-sort
+int compare_function(const void *a,const void *b) {
+    int *x = (int *) a;
+    int *y = (int *) b;
+    return *x - *y;
+}
+
+int main(){
+
+    FILE *fpr, *fpw;
+
+    fpr = fopen("./log.txt", "r");
+    if (fpr==NULL){
+        printf("Failed to open file\n");
+        return 0;
+    }
+
+    //https://stackoverflow.com/questions/14001907/read-data-from-file-till-end-of-line-in-c-c
+
+    char buffer[10000];
+    char *pbuff;
+    int value;
+    
+    if (!fgets(buffer, sizeof buffer, fpr)) return 0;
+    pbuff = buffer;
+    int n = strtol(pbuff, &pbuff, 10);
+
+    int numbers[n];
+    for (int i=0; i<n; i++){
+        if (!fgets(buffer, sizeof buffer, fpr)) break;
+        pbuff = buffer;
+        numbers[i] = strtol(pbuff, &pbuff, 10);
+    }
+    
+    // for (int i=0; i<n; i++){
+    //     printf("numbers[%d] = %d\n", i,numbers[i]);
+    // }
+
+    fclose(fpr);
+
+
+    // https://stackoverflow.com/questions/1787996/c-library-function-to-perform-sort
+    // qsort(<arrayname>,<size>,sizeof(<elementsize>),compare_function);
+    qsort(numbers, n, sizeof(*numbers), compare_function);
+    for (int i=0; i<n; i++){
+        printf("numbers[%d] = %d\n", i,numbers[i]);
+    }
+
+    fpw = fopen("./sorted.txt", "w");
+    if (fpr==NULL){
+        printf("Failed to open file\n");
+        return 0;
+    }
+
+    fprintf(fpw, "%d\n", n);
+    for (int i=0; i<n; i++){
+        fprintf(fpw, "%d\n", numbers[i]);
+    }
+
+    fclose(fpw);
+    fpw = NULL;
+    
+    return 0;
+}
