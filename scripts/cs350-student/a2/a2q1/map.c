a94995348ad02a0b204395ac0fc69acccc49a6d1
owidjaja <owidjaja@student.cs.uwaterloo.ca>
2022-02-27 15:48:18 -0500

    code a2q1 map.c: multithreading
    multithreading to acccess library arrays
    
    problem was in splitting up array for each threads
    stuck on int/float conversion bug
    
    used global variables for library, word
    alternative is using argument struct to pass in arg
    
    was unsure about malloc, free in loops
    
    printf race condition? seems not, but yes for c++ cout

--- scripts/cs350-student/a2/a2q1/map.c
+++ scripts/cs350-student/a2/a2q1/map.c
@@ -28,11 +28,72 @@
  * --------------------------------------------------------------------
  */
 
+struct Library *global_lib;
+char *global_word;
+int chunk_size;
+
+void *thread_handler(void *);
+
 size_t MultithreadedWordCount( struct  Library * lib, char * word)
 {
   printf("Parallelizing with %d threads...\n",NUMTHREADS);
-    /* XXX FILLMEIN
-     * Also feel free to remove the printf statement
-     * to improve time */
-    return 0;
+    
+    global_lib = lib;
+    global_word = word;
+    chunk_size = lib->numArticles / NUMTHREADS;
+
+    pthread_t threads[NUMTHREADS];
+    unsigned int i;
+
+    for (i=0; i<NUMTHREADS; i++){
+      int *index = malloc(sizeof(unsigned int));
+      *index = i;
+      if (pthread_create(&threads[i], NULL, &thread_handler, index) != 0) {
+          perror("Failed to create thread");
+      }
+    }
+
+    size_t total_sum = 0;
+    for (i=0; i<NUMTHREADS; i++){
+      int *ret;
+      if (pthread_join(threads[i], (void **) &ret) != 0){
+        perror("Failed to join thread");
+      }
+      total_sum += *ret;
+      free(ret);
+    }
+
+    // printf("total_sum: %ld\n", total_sum);
+    return total_sum;
+}
+
+void *thread_handler(void *arg){
+  int thread_index = *((int *)arg);
+
+    unsigned int start_index = (thread_index * chunk_size);
+    unsigned int end_index = start_index + chunk_size;
+    if (thread_index == NUMTHREADS - 1){
+        end_index = global_lib->numArticles;
+    }
+
+    // printf("Index: %d\n", thread_index);
+    // printf("Start Index: %d\n", start_index);
+    // printf("End Index: %d\n", end_index);
+
+    size_t local_count = 0;
+    for(unsigned int i =  start_index; i < end_index;  i++)
+    {
+        // printf("Processing array element at: %d\n", i);
+
+        struct Article * art = global_lib->articles[i];
+        for ( unsigned int j = 0; j < art->numWords; j++){
+          size_t len = strnlen( art->words[j], MAXWORDSIZE );
+          if ( !strncmp( art->words[j], global_word, len)){
+            local_count += 1;
+          }
+        }
+    }
+
+    *((int*)arg) = local_count;
+    return arg;
 }
