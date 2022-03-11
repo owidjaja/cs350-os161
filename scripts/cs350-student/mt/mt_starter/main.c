83815ec655f447fb908f147ed6cc60827f2e078d
owidjaja <owidjaja@uwaterloo.ca>
2022-03-11 09:47:07 -0500

    add background proc code
    ampersand recognized if last token
    modify argv to move NULL forward after & detected
    
    unsure waitpid parameters: pid={0,-1}, options=WNOHANG

--- scripts/cs350-student/mt/mt_starter/main.c
+++ scripts/cs350-student/mt/mt_starter/main.c
@@ -25,7 +25,7 @@ void throw_err(const char *errmsg){
 	// exit(1);
 }
 
-void parse_input(char **sh_argv, FILE *line){
+int parse_input(char **sh_argv, FILE *line){
 	char cmdline[MAX_CMD_LEN];
 	fgets(cmdline, MAX_CMD_LEN, line);
 
@@ -36,6 +36,7 @@ void parse_input(char **sh_argv, FILE *line){
 		token = strtok(NULL, DELIMITER);
 	}
 	sh_argv[sh_argc] = NULL;
+	return sh_argc;
 }
 
 // Section for built-in commands
@@ -65,7 +66,8 @@ int sh_pwd(){
 }
 
 int sh_wait(){
-	// pending background jobs
+	// wait for any child with same process group to terminate
+	waitpid(-1, NULL, 0);
 	return 0;
 }
 
@@ -90,11 +92,17 @@ int (*sh_builtin_func[]) (char **) = {
 
 // End section
 
-int doexec(char **sh_argv){
+int doexec(int sh_argc, char **sh_argv){
 	for (int i=0; i<sh_num_builtins(); i++){
 		if (strcmp(sh_argv[0], sh_builtin[i]) == 0){
 			return (*sh_builtin_func[i])(sh_argv);
 			}
+	}
+
+	int is_background = 0;
+	if (strcmp(sh_argv[sh_argc-1], "&") == 0){
+		is_background = 1;
+		sh_argv[sh_argc-1] = NULL;
 	}
 
 	// if not built-in
@@ -106,7 +114,9 @@ int doexec(char **sh_argv){
 		case 0:
 			return execvp(sh_argv[0], sh_argv);
 		default:
-			waitpid(pid, NULL, 0); break;
+			if (!is_background){
+				waitpid(pid, NULL, 0); break;
+			}
 	}
 
 	return -1;
@@ -116,10 +126,10 @@ int doexec(char **sh_argv){
 int main( int argc, char ** argv ){
 	char *sh_argv[MAX_SH_ARG];
 
-	for (int status = 0; status != -1;){
+	for (int status = 0; status != -1; status=0){
 		printf("> ");
-		parse_input(sh_argv, stdin);
-		doexec(sh_argv);
+		int sh_argc = parse_input(sh_argv, stdin);
+		doexec(sh_argc, sh_argv);
 	}
 
 	throw_err("outside main loop");
