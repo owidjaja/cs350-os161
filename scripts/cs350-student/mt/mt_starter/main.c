3d946eb69369f8190c0057f9a3b7adc8bb1dbb79
owidjaja <owidjaja@uwaterloo.ca>
2022-03-19 04:06:36 -0400

    add cd HOME, replace printf with HOME
    random changes to status, have to revisit return codes

--- scripts/cs350-student/mt/mt_starter/main.c
+++ scripts/cs350-student/mt/mt_starter/main.c
@@ -18,11 +18,14 @@
 #define MAX_SH_ARG 64
 #define DELIMITER " \t\n"
 
-void throw_err(const char *errmsg){
+int throw_err(const char *errmsg){
 	// perror(errmsg);		// uncomment to see written printf error msgs
 	const char error_message[30] = "An error has occured \n";
 	write(STDERR_FILENO, error_message, strlen(error_message));
+
+	// choose one
 	exit(1);
+	return -1;
 }
 
 // parses cmdline into tokens in sh_argv
@@ -53,8 +56,12 @@ int sh_num_builtins(){
 }
 
 int sh_cd(char **argv){
+	if (argv[1] == NULL){
+		chdir(getenv("HOME"));
+		return 0;
+	}
 	if (chdir(argv[1]) != 0){
-		throw_err("chdir");
+		return throw_err("chdir");
 	}
 	return 0;
 }
@@ -62,7 +69,7 @@ int sh_cd(char **argv){
 int sh_pwd(){
 	char cwd[1024];
     getcwd(cwd, sizeof(cwd));
-    printf("%s\n", cwd);
+	write(STDOUT_FILENO, cwd, 1024);
 	return 0;
 }
 
@@ -110,11 +117,12 @@ int doexec(int sh_argc, char **sh_argv){
 	pid_t pid = fork();
 	switch(pid){
 		case -1:
-			throw_err("fork");
+			return throw_err("fork");
 			break;
 		case 0:
 			if (execvp(sh_argv[0], sh_argv) == -1){
-				throw_err("execvp, invalid cmd");
+				write(STDOUT_FILENO, "execvp\n", 10);
+				return throw_err("execvp, invalid cmd");
 			}
 		default:
 			if (!is_background){
@@ -122,7 +130,7 @@ int doexec(int sh_argc, char **sh_argv){
 			}
 	}
 
-	return -1;
+	return 0;
 }
 
 
@@ -132,27 +140,34 @@ int main( int argc, char ** argv ){
 
 	switch(argc){
 		case 1:
-			for (int status = 0; status != -1; status=0){
-				printf("> ");
+			printf("Interactive mode\n");
+			for (int status = 0; status != -1;){
+				write(STDOUT_FILENO, "> ", 3);
 				fgets(cmdline, MAX_CMD_LEN, stdin);
 				int sh_argc = tokenize_input(cmdline, sh_argv);
-				doexec(sh_argc, sh_argv);
+				status = doexec(sh_argc, sh_argv);
+				// printf("main status = %d\n", status);
 			}
 			break;
 		case 2:
 			;
+			printf("Batch mode\n");
 			FILE *file_ptr = fopen(argv[1], "r");
 			while (fgets(cmdline, MAX_CMD_LEN, file_ptr)){
-				write(STDOUT_FILENO, cmdline, strlen(cmdline));
+				// write(STDOUT_FILENO, cmdline, strlen(cmdline));
 				int sh_argc = tokenize_input(cmdline, sh_argv);
-				doexec(sh_argc, sh_argv);
+				
+				if (doexec(sh_argc, sh_argv) == -1){
+					// return throw_err("batch status");
+					break;
+				}
 			}
 			break;
 		default:
-			throw_err("Usage: ./myshell [batchFile]");
+			return throw_err("Usage: ./myshell [batchFile]");
 			break;
 	}
 
-	// throw_err("outside main loop");
+	throw_err("exit status");
 	return 0;
 }
